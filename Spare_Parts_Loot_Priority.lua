06f3cdc37f0e918d82d4da10c00c57ec0a3da4d5
Sansaan <67605814+Sansaan@users.noreply.github.com>
2022-04-13 08:54:44 -0400

    Initial commit

---
+++ Spare_Parts_Loot_Priority.lua
@@ -0,0 +1,64 @@
+function ItemSearch(itemname)
+    for index, value in next, spare_parts_loot_table do
+        if value["loot_id"] == itemname then
+            return value["prio"]
+        end
+    end
+end
+
+function split (str, pat, limit)
+  local t = {}
+  local fpat = "(.-)" .. pat
+  local last_end = 1
+  local s, e, cap = str:find(fpat, 1)
+  while s do
+    if s ~= 1 or cap ~= "" then
+      table.insert(t, cap)
+    end
+
+    last_end = e+1
+    s, e, cap = str:find(fpat, last_end)
+
+    if limit ~= nil and limit <= #t then
+      break
+    end
+  end
+
+  if last_end <= #str then
+    cap = str:sub(last_end)
+    table.insert(t, cap)
+  end
+
+  return t
+end
+
+function MakeTooltip(tooltip, ...)
+  local itemname, itemlink = tooltip:GetItem()
+  
+  if itemlink then
+    priority = ItemSearch(itemlink:match("item:(%d+):"))
+
+    color_dots = "47FAF6"
+    color_title = "FA83F5"
+    color_prio = "ffa60c"
+    
+    if priority then
+      headfoot = string.format("|c00"..color_dots.."*****************************")
+      spacer = string.format("|c00"..color_dots.."*")
+      title = string.format("|c00"..color_dots.."*|c00"..color_title.."  Spare Parts Loot Priority:")
+      tooltip:AddLine(headfoot)
+      tooltip:AddLine(title)
+
+      priolist = split(priority, " > ")
+      for i,prio in ipairs(priolist) do
+        tooltip:AddLine(string.format("|c00"..color_dots.."*|c00"..color_prio.."      %s", prio))
+      end
+
+      tooltip:AddLine(spacer)
+      tooltip:AddLine(headfoot)
+    end
+  end
+end
+
+GameTooltip:HookScript("OnTooltipSetItem", MakeTooltip)
+ItemRefTooltip:HookScript("OnTooltipSetItem", MakeTooltip)
